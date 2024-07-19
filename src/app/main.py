4d5368fe85a23ba13fd2c6b183bb1658ecc65308
Benjamin Donnaloia <bdonz@Benjamins-Mini.lan>
2024-07-18 21:51:24 -0600

    Added basic logging

--- src/app/main.py
+++ src/app/main.py
@@ -4,6 +4,8 @@ import psycopg2
 from src.db import models
 from src.db.database import engine
 from src.router import items, categories
+from src.app.middleware import log_middleware
+from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi_pagination import add_pagination
 
 
@@ -11,6 +13,7 @@ from fastapi_pagination import add_pagination
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(BaseHTTPMiddleware, dispatch=log_middleware)
 app.include_router(categories.router)
 app.include_router(items.router)
 
